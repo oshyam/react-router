9e2efbde9e5e85a82b86a720d40b01fc38df4e8f
oshyam <myworkplaceservice@gmail.com>
2020-11-08 20:52:53 +0530

    Added example of React Router

---
+++ src/User.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function User() {
+    return (
+        <div>
+            <h1>User Component Demo</h1>
+       
+
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//react-router
+import {Route,Link,Switch,BrowserRouter as Router} from "react-router-dom";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//importing pages 
+import User from "./User"
+import notFound from './notFound';
+import visit from './visit';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//make a component for route
+//Making Route for each pages <Route path="/" component={App} />
+//exact helps in routing to find excat url
+//Switch if no route found it will display default route
+//Link as for routing - directly navigate to the directory
+//we can separate the div of link in separate file and access route comp from another file
+ 
+const routing = (
+  <Router>
+  <div>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/user">User</Link></li>
+      <li><Link to="/visit">Visit</Link></li>
+
+    </ul>
+  </div>
+  <Switch>
+  <Route exact path="/" component={App} />
+  <Route path="/user" component={User} />
+  <Route path="/notfound" component={notFound} />
+  <Route path="/visit" component={visit} />
+  <Route  component={notFound} />
+  </Switch>
+  </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+ReactDOM.render(
+  routing,
+  document.getElementById('root')
+);
